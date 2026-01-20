04c3d7017a0964513472e03073c4947536a6a0ff
Narvin Sachaniya <githubnarvin@gmail.com>
2026-01-20 10:26:45 +0530

    Add PWA install prompt to global providers

--- urjaflow-app/components/Providers.tsx
+++ urjaflow-app/components/Providers.tsx
@@ -2,11 +2,13 @@
 
 import { SessionProvider } from "next-auth/react";
 import { NotificationProvider } from "@/components/Notification";
+import PWAInstallPrompt from "@/components/PWAInstallPrompt";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <NotificationProvider>
+        <PWAInstallPrompt />
         {children}
       </NotificationProvider>
     </SessionProvider>
